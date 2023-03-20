811f5fa46bdfa579f78e877b5e2ea607b1ea313e
Daniel Bonilha <dani.bonilha@gmail.com>
2023-03-20 14:09:41 -0300

    added max accurrences solution in go

--- go/algorithms/max_occurrences.go
+++ go/algorithms/max_occurrences.go
@@ -1,7 +1,30 @@
 package algorithms
 
 func MaxOccurrences(arr []int, number int) int {
-	return -2
+	index := BinarySearch(number, arr)
+	if index == -1 {
+		return 0
+	}
+
+	count := 1
+
+	// count items to the left of the index
+	for i := index - 1; i >= 0; i-- {
+		if arr[i] != number {
+			break
+		}
+		count += 1
+	}
+
+	// count items to the right of the index
+	for i := index + 1; i < len(arr); i++ {
+		if arr[i] != number {
+			break
+		}
+		count += 1
+	}
+
+	return count
 }
 
 // Encontre o número de ocorrências de um
